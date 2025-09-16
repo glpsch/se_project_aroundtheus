7f8fc4a3d8ba2eae1948d6b471449e51f76d236e
Galina Psch <over.passover@gmail.com>
2025-09-16 03:07:36 +0300

    folder restructure

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-import { initialCards } from "./initialCards.js";
+import { initialCards } from "../components/initialCards.js";
 import {
   enableValidation,
   clearFormErrors,
   initializeFormState,
-} from "./validate.js";
+} from "../components/validate.js";
+import Card from "../components/Card.js";
 
 // DOM Elements - Popups
 const popupAddCard = document.querySelector(".popup_add-card");
@@ -31,7 +32,7 @@ const userAddPlaceButton = document.querySelector(".user-info__place-button");
 const placesList = document.querySelector(".places-list");
 
 
-////////////////////////////////////////////////////////////
+// Handling image click
 const handleImageClick = (card) => {
   const imgElement = imgDiv.querySelector("img");
   if (imgElement) {
@@ -45,59 +46,9 @@ const handleImageClick = (card) => {
   popImageDescription.textContent = card._name;
   openPopup(popupImage);
 };
+//////////////////////////////////////////////////////////
 
 
-class Card {
-  constructor(data, cardSelector, handleImageClick) {
-    this._name = data.name;
-    this._link = data.link;
-    this._template = cardSelector;
-    this._handleImageClick = handleImageClick;
-    this.isLiked = false;
-  }
-
-  _getTemplate() {
-    return this._template.content.querySelector('.place-card').cloneNode(true);
-  }
-
-  generateCard() {
-    this._element = this._getTemplate();
-    this._cardImageElement = this._element.querySelector('.place-card__image');
-    this._cardImageElement.style.backgroundImage = `url(${this._link})`;
-    this._element.querySelector('.place-card__name').textContent = this._name;    
-    this._setEventListeners();
-    return this._element;
-  }
-  
-  _handleLike() {
-    this.isLiked = !this.isLiked;
-    const likeButton = this._element.querySelector('.place-card__like-icon');
-    likeButton.classList.toggle('place-card__like-icon_liked');
-  }
-  
-  _handleDelete() {
-    this._element.remove();
-  }
-  
-  _setEventListeners() {
-    // Image click handler
-    this._cardImageElement.addEventListener('click', () => {
-      this._handleImageClick(this);
-    });    
-    
-    const likeButton = this._element.querySelector('.place-card__like-icon');
-    likeButton.addEventListener('click', (e) => {
-      e.stopPropagation(); // Prevent event bubbling
-      this._handleLike();
-    });
-    
-    const deleteButton = this._element.querySelector('.place-card__delete-icon');
-    deleteButton.addEventListener('click', (e) => {
-      e.stopPropagation(); // Prevent event bubbling
-      this._handleDelete();
-    });
-  }
-}
 
 ///
 
